f7aefa11f909f88f8c7c35fda1120e38e310611a
cjredmond <connor.redmond@gmail.com>
2017-03-09 15:20:56 -0500

    was able to access all 4 messages

--- app/adapters/message.js
+++ app/adapters/message.js
@@ -1,7 +1,7 @@
 import DS from 'ember-data';
 
 export default DS.JSONAPIAdapter.extend({
-  host: 'http://private-227b9-jsonapifizzbuzz.apiary-mock.com/messages',
+  host: 'http://private-227b9-jsonapifizzbuzz.apiary-mock.com',
 
   pathForType() {
     return 'messages';
