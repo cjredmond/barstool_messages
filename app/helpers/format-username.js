083ea822c17667b0248ae99af4a4e309ce8cfbd6
cjredmond <connor.redmond@gmail.com>
2017-03-10 11:01:36 -0500

    added helper to clean and title case names

---
+++ app/helpers/format-username.js
@@ -0,0 +1,20 @@
+import Ember from 'ember';
+
+export function formatUsername(params) {
+  console.log(params[0])
+  var splitNames = params[0].split('.');
+  var firstName = splitNames[0];
+  var lastName = splitNames[1];
+  var lastNameClean = lastName.replace(/\d+/g,'');
+
+  function toTitleCase(str) {
+    return str.replace(/\w\S*/g, function(txt){return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();});
+}
+
+  var firstNameTitle = toTitleCase(firstName)
+  var lastNameTitle = toTitleCase(lastNameClean)
+
+  return firstNameTitle + ' ' + lastNameTitle;
+}
+
+export default Ember.Helper.helper(formatUsername);
---
+++ tests/unit/helpers/format-username-test.js
@@ -0,0 +1,12 @@
+
+import { formatUsername } from 'barstool-messages/helpers/format-username';
+import { module, test } from 'qunit';
+
+module('Unit | Helper | format username');
+
+// Replace this with your real tests.
+test('it works', function(assert) {
+  let result = formatUsername([42]);
+  assert.ok(result);
+});
+
